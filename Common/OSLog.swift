8d7da373b69c2638cddc404c7d2e989ab0bb6ff4
bastiaanv <bastiankpn7800@gmail.com>
2024-01-03 20:19:12 +0100

    more logs

--- Common/OSLog.swift
+++ Common/OSLog.swift
@@ -42,7 +42,6 @@ extension OSLog {
             
             return allEntries
                 .compactMap { $0 as? OSLogEntryLog }
-                .filter { $0.subsystem == "com.randallknutson.DanaKit" }
                 .map({ "[\($0.date.formatted(date: .numeric, time: .shortened)) \(getLevel($0.level))] \($0.composedMessage)" })
                 .joined(separator: "\n")
         } catch {
