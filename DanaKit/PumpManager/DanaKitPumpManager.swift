f3175a150f0863e31ff3d1c1904b1f282eac709e
bastiaanv <bastiankpn7800@gmail.com>
2024-04-13 12:45:06 +0200

    fix: If something goes wrong when bolussing, let the ensurePumpData function deal with the data

--- DanaKit/PumpManager/DanaKitPumpManager.swift
+++ DanaKit/PumpManager/DanaKitPumpManager.swift
@@ -1292,13 +1292,8 @@ extension DanaKitPumpManager {
         self.doseEntry = nil
         self.doseReporter = nil
         
-        guard let dose = dose else {
-            return
-        }
-        
-        self.pumpDelegate.notify { (delegate) in
-            delegate?.pumpManager(self, hasNewPumpEvents: [NewPumpEvent.bolus(dose: dose, units: deliveredUnits)], lastReconciliation: self.state.lastStatusDate, replacePendingEvents: true,  completion: { _ in })
-        }
+        // We dont store the bolus or anything
+        // The ensurePumpData will make sure everything is up-to-date
     }
     
     func notifyBolusDidUpdate(deliveredUnits: Double) {
