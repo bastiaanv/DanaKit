918e311f735ddcded70c67dfb6de301184ea0a71
Bastiaan Verhaar <3987804+bastiaanv@users.noreply.github.com>
2026-03-30 08:49:55 +0200

    chore: update translation key

--- DanaKitUI/Views/Onboarding/InsulinTypeConfirmation.swift
+++ DanaKitUI/Views/Onboarding/InsulinTypeConfirmation.swift
@@ -37,24 +37,7 @@ struct InsulinTypeConfirmation: View {
         }
         .edgesIgnoringSafeArea(.bottom)
         .navigationBarHidden(false)
-        .navigationTitle(LocalizedString("Insulin type", comment: "Title for insulin type"))
-    }
-
-    @ViewBuilder private var title: some View {
-        Text(LocalizedString("Select insulin type", comment: "Title for insulin type"))
-            .font(.title)
-            .bold()
-            .padding([.bottom, .horizontal])
-
-        Text(LocalizedString(
-            "Select the type of insulin that you will be using in this pump.",
-            comment: "Title text for insulin type confirmation page"
-        ))
-            .fixedSize(horizontal: false, vertical: true)
-            .padding(.horizontal)
-
-        Divider()
-            .padding(.vertical)
+        .navigationTitle(LocalizedString("Insulin Type", comment: "Title for insulin type"))
     }
 }
 
